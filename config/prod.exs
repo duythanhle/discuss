1b01e670f7859e7e5fe7ab6792cbba200282332c
Thanh Le <thanhle@Thanhs-MacBook-Air.local>
2022-03-04 22:50:34 +0700

    add debug

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,11 @@ config :disscuss, DisscussWeb.Endpoint, cache_static_manifest: "priv/static/cach
   url: [scheme: "https", host: "stormy-thicket-54185.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  check_origin: [
+      "//stormy-thicket-54185.herokuapp.com",
+      "//stormy-thicket-54185.herokuapp*"
+    ]
 
 # Do not print debug messages in production
 config :logger, level: :info
